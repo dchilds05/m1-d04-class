be52311c4a239ab268381afcb7078cd96941d728
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-03 16:23:57 +0200

    WIP step 1

---
+++ 05_code-js_map_reduce_filter.js
@@ -0,0 +1,15 @@
+const bases = [3, 5, 7, 9, 11];
+// we want to have this as a result [9, 25, 49, 81, 121];
+
+const inputs = ["MaAcO", "fEdE"];
+const names = ["Marco", "Fede"];
+
+const myPlusOnes = bases
+  .map(function (element) {
+    console.log(`I am + 1 ${element} to get ${element + 1}`);
+    return element + 1;
+  })
+  .sort(); // map returns a CHAINABLE NEW array
+
+console.log(myPlusOnes);
+console.log(bases);
